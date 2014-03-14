4e28ebd4dacdc6ccb6f35fd8fc79b3972ef37779
Odonno <bottiau.david@laposte.net>
2014-03-14 16:42:30 +0100

    Implement ParkView with IViewModel property

--- Tamazombie/src/com/tamazombie/concreteView/ParkView.java
+++ Tamazombie/src/com/tamazombie/concreteView/ParkView.java
@@ -1,5 +1,7 @@
 package com.tamazombie.concreteView;
 
+import com.tamazombie.abstractLogic.IParkViewModel;
+import com.tamazombie.abstractLogic.IViewModel;
 import com.tamazombie.abstractView.IGameView;
 
 /**
@@ -10,4 +12,14 @@ import com.tamazombie.abstractView.IGameView;
  * To change this template use File | Settings | File Templates.
  */
 public class ParkView implements IGameView {
+    private IParkViewModel _parkViewModel;
+
+    public ParkView(IParkViewModel parkViewModel){
+        _parkViewModel = parkViewModel;
+    }
+
+    @Override
+    public IViewModel GetViewModel() {
+        return _parkViewModel;
+    }
 }
