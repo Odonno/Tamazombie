1b96e96536f644bd3e7617dccca9c02e61d9a6a9
Odonno <bottiau.david@laposte.net>
2014-03-25 16:57:37 +0100

    Refactoring textures of the ParkView. Add Dispose method to GameView.

--- Tamazombie/src/com/tamazombie/concreteView/ParkView.java
+++ Tamazombie/src/com/tamazombie/concreteView/ParkView.java
@@ -17,10 +17,17 @@ import com.tamazombie.abstractView.IParkView;
  */
 public final class ParkView implements IParkView {
     private IParkViewModel _parkViewModel;
+
     private IBackground _background;
     private IButton _buttonFeed;
     private IButton _buttonAmuse;
     private IButton _buttonTown;
+
+    private Texture _zombieTexture;
+    private Texture _backgroundTexture;
+    private Texture _buttonFeedTexture;
+    private Texture _buttonAmuseTexture;
+    private Texture _buttonTownTexture;
 
     public ParkView(IParkViewModel parkViewModel, IBackground background, IButton buttonFeed, IButton buttonAmuse, IButton buttonTown){
         _parkViewModel = parkViewModel;
@@ -37,21 +44,30 @@ public final class ParkView implements IParkView {
 
     @Override
     public void Create() {
+        // Create textures
+        _zombieTexture = new Texture(Gdx.files.internal("players/zombie.png"));
+        _backgroundTexture = new Texture(Gdx.files.internal("backgrounds/background.png"));
+        _buttonFeedTexture = new Texture(Gdx.files.internal("buttons/buttonFeed.png"));
+        _buttonAmuseTexture = new Texture(Gdx.files.internal("buttons/buttonAmuse.png"));
+        _buttonTownTexture = new Texture(Gdx.files.internal("buttons/buttonTown.png"));
+
+        // Set textures
+
         // set texture to player
-        _parkViewModel.getPlayer().setTexture(new Texture(Gdx.files.internal("players/zombie.png")));
+        _parkViewModel.getPlayer().setTexture(_zombieTexture);
         _parkViewModel.getPlayer().setScale(0.15f);
 
         // set texture to background
-        _background.setTexture(new Texture(Gdx.files.internal("backgrounds/background.png")));
+        _background.setTexture(_backgroundTexture);
 
         // set textures to buttons
-        _buttonFeed.setTexture(new Texture(Gdx.files.internal("buttons/buttonFeed.png")));
+        _buttonFeed.setTexture(_buttonFeedTexture);
         _buttonFeed.setPosition(-150, 120);
 
-        _buttonAmuse.setTexture(new Texture(Gdx.files.internal("buttons/buttonAmuse.png")));
+        _buttonAmuse.setTexture(_buttonAmuseTexture);
         _buttonAmuse.setPosition(40, 120);
 
-        _buttonTown.setTexture(new Texture(Gdx.files.internal("buttons/buttonTown.png")));
+        _buttonTown.setTexture(_buttonTownTexture);
         _buttonTown.setPosition(230, 120);
     }
 
@@ -61,8 +77,7 @@ public final class ParkView implements IParkView {
         int x = Gdx.input.getX();
         int y = Gdx.input.getY();
 
-        // TODO : Update buttons
-
+        // Update buttons
         _buttonFeed.IsHover(x, y);
         _buttonAmuse.IsHover(x, y);
         _buttonTown.IsHover(x, y);
@@ -72,7 +87,7 @@ public final class ParkView implements IParkView {
 
     @Override
     public void Draw(SpriteBatch spriteBatch) {
-        // TODO : Draw textures (images), play sound, ...
+        // Draw textures (images), play sound, ...
         _background.draw(spriteBatch);
 
         _buttonFeed.draw(spriteBatch);
@@ -80,5 +95,15 @@ public final class ParkView implements IParkView {
         _buttonTown.draw(spriteBatch);
 
         _parkViewModel.getPlayer().draw(spriteBatch);
+    }
+
+    @Override
+    public void Dispose() {
+        // Dispose textures, sounds, ...
+        _zombieTexture.dispose();
+        _backgroundTexture.dispose();
+        _buttonFeedTexture.dispose();
+        _buttonAmuseTexture.dispose();
+        _buttonTownTexture.dispose();
     }
 }
