4d31247876113233dc652d56aa04912cf441b6a7
Odonno <bottiau.david@laposte.net>
2014-04-02 13:45:05 +0200

    Add ITownViewModel

---
+++ Tamazombie/Logic/src/com/tamazombie/abstractLogic/ITownViewModel.java
@@ -0,0 +1,18 @@
+package com.tamazombie.abstractLogic;
+
+import com.tamazombie.abstractModel.IPlayer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: David Bottiau
+ * Date: 02/04/14
+ * Time: 13:31
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ITownViewModel {
+    public IPlayer GetPlayer();
+
+    public void AddEnemy();
+    public void Fight();
+    public void Reply();
+}
